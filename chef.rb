183ef5c1d7426d8c96189e13b8223d3bd9985258
Fernando J Pando <fernando@pubnub.com>
2015-02-10 12:12:42 -0500

    added chef recipe

---
+++ chef.rb
@@ -0,0 +1,12 @@
+package 'nginx' do
+  action :install
+end
+
+service 'nginx' do
+  action [ :enable, :start ]
+end
+
+cookbook_file "/usr/share/nginx/html/index.html" do
+  source "index.html"
+  mode "0644"
+end
