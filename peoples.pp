e7389c49422a1cc965b10b04e2c3bd9181de4d16
nando p <nando@hex7.com>
2015-02-10 10:19:42 -0500

    nginx service

--- peoples.pp
+++ peoples.pp
@@ -2,5 +2,5 @@ class peoples::automation {
 	package { "epel-release: ensure => "installed" }
 	package { "nginx: ensure => "installed", Require => Package["epel-release"] }
 	file { "/usr/share/nginx/html/index.html": source  => "puppet:///modules/peoples/index.html" }
-        exec { "service nginx start" }
+        service { "nginx": ensure => running }
 }
