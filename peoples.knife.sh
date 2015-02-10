6074bb52c5ec0edec6a1e5de7be44d529622cde4
nando p <nando@hex7.com>
2015-02-10 13:41:39 -0500

    knife with puppet

--- peoples.knife.sh
+++ peoples.knife.sh
@@ -2,4 +2,4 @@
 
 knife ec2 server create --groups=default --region=us-east-1 --availability-zone=us-east-1a --image=ami-895069fd --flavor=t1.micro --ssh-user=root --ssh-key=peoples --identity-file=~/peoples
 
-puppet agent -t -server puppetmaster.fqdn
+ssh -i peoples root@thishost "puppet agent -t -server puppetmaster.fqdn"
